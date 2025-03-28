62e6b27c177932aaa8ec8933c37779c7dfab066c
Shiwani <shiwani.soni@codeclouds.co.in>
2025-03-28 11:40:50 +0530

    sqlite db added

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+def istnow():
+    return datetime.utc_now + timedelta(hours=5, minutes=30)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=istnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
